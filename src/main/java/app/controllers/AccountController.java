f3d992aa965dcda0a589353bae23d50955c2efec
maryRom01 <tester.automation.11@gmail.com>
2024-07-20 08:24:24 -0400

    minor (cleanup)

--- src/main/java/app/controllers/AccountController.java
+++ src/main/java/app/controllers/AccountController.java
@@ -8,10 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/app/controllers/CustomerController.java
+++ src/main/java/app/controllers/CustomerController.java
@@ -7,8 +7,6 @@ import app.services.serviceInterface.AccountService;
 import app.services.serviceInterface.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
